d61944caa98016b6c512ce56b22b1bc85f1751a6
Nick Wishnyakov <nick@nobazar.ru>
2014-09-23 01:16:38 +0400

    хелперы для php storm и дополнительные команды для artisan

--- app/config/app.php
+++ app/config/app.php
@@ -122,6 +122,8 @@ return array(
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
         'Slushie\LaravelAssetic\LaravelAsseticServiceProvider',
+        'Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider',
+        'Way\Generators\GeneratorsServiceProvider',
 
 	),
 
